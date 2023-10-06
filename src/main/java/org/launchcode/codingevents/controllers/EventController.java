1fb3de9715f30a86bf0b3b56d4a861e12ad5e032
varti2023 <varti.pant@gmail.com>
2023-10-05 23:45:01 -0500

    model binding

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,10 +4,7 @@ import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -35,10 +32,9 @@ public class EventController {
 
     // lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription
-                              ,@RequestParam String eventLocation){
+    public String createEvent(@ModelAttribute Event newEvent){
 
-        EventData.add(new Event(eventName,eventDescription,eventLocation));
+        EventData.add(newEvent);
         return "redirect:/events";
     }
 
