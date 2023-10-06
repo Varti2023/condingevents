8894e9bfd0f1d68a3a0516f6a34509e948419f8e
varti2023 <varti.pant@gmail.com>
2023-10-05 23:30:18 -0500

    create and delete event

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,5 +1,7 @@
 package org.launchcode.codingevents.controllers;
 
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,26 +9,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Controller
-@RequestMapping("/events")
+@RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
-    @GetMapping("")
+   // private static List<Event> events = new ArrayList<>();
+    @GetMapping
     public String displayAllEvents(Model model){
 
-
-//        List<String> events = new ArrayList<>();
-//        events.add("Code With Pride");
-//        events.add("Strange Loop");
-//        events.add("Apple WWDC");
-//        events.add("SpringOne Platform");
-//        events.add("Thymeleaf Template");
         model.addAttribute("title", "All Events");
-        model.addAttribute("events", events);
+        model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
     // lives at /events/create
@@ -38,9 +35,27 @@ public class EventController {
 
     // lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName){
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription
+                              ,@RequestParam String eventLocation){
 
-        events.add(eventName);
+        EventData.add(new Event(eventName,eventDescription,eventLocation));
         return "redirect:/events";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEvent(Model model){
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String deleteEvent(@RequestParam (required = false) int[] eventIds){
+        if(eventIds!=null) {
+            for (int id : eventIds) {
+                EventData.removeEvent(id);
+            }
+        }
+          return "redirect:/events";
     }
 }
---
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -0,0 +1,69 @@
+package org.launchcode.codingevents.models;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Objects;
+
+public class Event {
+
+    private int id;
+    private static int nextId=1;
+    private String name;
+    private String description;
+    private String location;
+
+    public Event(String name, String description, String location) {
+        this.name = name;
+        this.description=description;
+        this.location=location;
+        this.id=nextId;
+        nextId++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
