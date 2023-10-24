7132bca172c2dca4f7789b78a19cc237854c6164
varti2023 <varti.pant@gmail.com>
2023-10-24 00:11:57 -0500

    changes

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,10 +1,16 @@
 package org.launchcode.codingevents.controllers;
 
+import jakarta.validation.Valid;
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.EventType;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -27,13 +33,19 @@ public class EventController {
    @GetMapping("create")
     public String renderCreateEventForm(Model model){
        model.addAttribute("title", "Create Event");
+       model.addAttribute(new Event());
+       model.addAttribute("types",EventType.values());
         return "events/create";
     }
 
     // lives at /events/create
     @PostMapping("create")
-    public String createEvent(@ModelAttribute Event newEvent){
+    public String createEvent(@ModelAttribute @Valid Event newEvent, Errors errors, Model model){
 
+        if(errors.hasErrors()){
+            model.addAttribute("title", "Create Event");
+            return "events/create";
+        }
         EventData.add(newEvent);
         return "redirect:/events";
     }
@@ -64,11 +76,13 @@ public class EventController {
         return "events/edit";
     }
     @PostMapping("edit")
-    public String processEditForm(int eventId, String name, String description) {
+    public String processEditForm(int eventId, String name, String description,String location, String contactEmail) {
 
         Event eventEdited  = EventData.getById(eventId);
         eventEdited.setName(name);
         eventEdited.setDescription(description);
+        eventEdited.setLocation(location);
+        eventEdited.setContactEmail(contactEmail);
         return "redirect:/events";
     }
 }
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,27 +1,57 @@
 package org.launchcode.codingevents.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.validation.annotation.Validated;
+
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Objects;
+import jakarta.validation.constraints.Size;
+
 
 public class Event {
 
     private int id;
     private static int nextId=1;
+
+    @NotBlank(message="Name is required")
+    @Size(min =3, max=50 , message="Name must be between 3 to 50 characters.")
     private String name;
+    @Size(max=500, message = "Description too long.")
     private String description;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid Email. Try again")
+    private String contactEmail;
+
+    private EventType type;
     private String location;
 
-    public Event(String name, String description, String location) {
+    public Event() {
+    }
+
+    public Event(String name, String description, String location, String contactEmail, EventType type) {
         this.name = name;
         this.description=description;
         this.location=location;
+        this.contactEmail=contactEmail;
         this.id=nextId;
+        this.type = type;
         nextId++;
     }
 
     public int getId() {
         return id;
+    }
+
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
     }
 
     public String getName() {
@@ -47,6 +77,14 @@ public class Event {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     @Override
